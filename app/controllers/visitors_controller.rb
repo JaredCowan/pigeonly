7749f2feddc90f2d4341bbfb4142303ecca89676
JaredCowan <jaredlc@outlook.com>
2016-06-09 03:17:39 -0700

    change regex

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,10 +2,10 @@ class VisitorsController < ApplicationController
 
   def regex
     numbers = []
-    body_param = params[:post][:body].gsub(/[a-zA-Z.-]/, '').strip.split(' ')
+    body_param = params[:post][:body].gsub(/([a-zA-Z.-])/, '').strip.split(' ')
 
     body_param.each do |n|
-      numbers.push(n.match(/\d{0,}[\d]{10,}/))
+      numbers.push(n.match(/[\d]{10}/))
     end
 
     @body = numbers.compact
