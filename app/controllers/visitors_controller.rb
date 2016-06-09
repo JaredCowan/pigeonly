f80600cbb85b5474c22c8af0077232fb9ab546ce
JaredCowan <jaredlc@outlook.com>
2016-06-09 00:43:32 -0700

    renamed variable

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,9 +2,9 @@ class VisitorsController < ApplicationController
 
   def regex
     numbers = []
-    bodyParam = params[:post][:body].gsub(/[a-zA-Z.-]/, '').strip.split(' ')
+    body_param = params[:post][:body].gsub(/[a-zA-Z.-]/, '').strip.split(' ')
 
-    bodyParam.each do |n|
+    body_param.each do |n|
       numbers.push(n.match(/\d{0,}[\d]{10,}/))
     end
 
